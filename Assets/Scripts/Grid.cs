8264513d8ada55973be762aa51973a0d669a5f23
Sam Feast <sfeast43@gmail.com>
2024-04-04 19:38:02 +0100

    Perlin noise to determine land/water state

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    public int size = 256;
+    public int size;
+    public float waterLevel;
 
     public Cell[,] grid;
 
@@ -14,18 +15,24 @@ public class Grid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        System.Random rand = new System.Random();
+
+        System.Random rng = new System.Random();
+        int xOrigin = rng.Next(-65536, 65536);
+        int yOrigin = rng.Next(-65536, 65536);
+
+        float[,] noiseMap = new float[size, size];
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                float noiseValue = Mathf.PerlinNoise((float) (8 * (x + xOrigin)) / size, (float) (8 * (y + yOrigin)) / size);
+                noiseMap[x, y] = noiseValue;
+            }
+        }
+
         grid = new Cell[size, size];
         for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y ++) {
+            for (int y = 0; y < size; y++) {
                 Cell cell = new Cell();
-                int randno = rand.Next(0, 2);
-                print(randno);
-                if (randno == 1) {
-                    cell.isWater = true;
-                } else {
-                    cell.isWater = false;
-                }
+                cell.isWater = noiseMap[x, y] < waterLevel;
                 grid[x, y] = cell;
             }
         }
