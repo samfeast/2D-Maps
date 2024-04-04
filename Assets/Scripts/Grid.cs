7b640432b8e35cdc7e89cd443cb9977d364e0be8
Sam Feast <sfeast43@gmail.com>
2024-04-04 20:15:03 +0100

    Beaches (outlines) around land masses

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -10,6 +10,7 @@ public class Grid : MonoBehaviour
     public float waterLevel;
 
     public Cell[,] grid;
+    public float[,] noiseMap;
 
 
     // Start is called before the first frame update
@@ -20,7 +21,7 @@ public class Grid : MonoBehaviour
         int xOrigin = rng.Next(-65536, 65536);
         int yOrigin = rng.Next(-65536, 65536);
 
-        float[,] noiseMap = new float[size, size];
+        noiseMap = new float[size, size];
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 float noiseValue = Mathf.PerlinNoise((float) (8 * (x + xOrigin)) / size, (float) (8 * (y + yOrigin)) / size);
@@ -48,12 +49,30 @@ public class Grid : MonoBehaviour
                 Cell cell = grid[x, y];
                 if (cell.isWater) {
                     Gizmos.color = Color.blue;
+                } else if (AdjacentWater(x, y)) {
+                    Gizmos.color = Color.yellow;
                 } else {
                     Gizmos.color = Color.green;
                 }
                 Vector3 pos = new Vector3(x * 0.1f, y * 0.1f, 0);
                 Gizmos.DrawCube(pos, 0.1f * Vector3.one);
             }
+        }
+    }
+
+    bool AdjacentWater(int x, int y) {
+        if (y == size - 1 || noiseMap[x, y+1] < waterLevel) {
+            return true;
+        } else if (x == size - 1 || noiseMap[x+1, y] < waterLevel) {
+            return true;
+        }
+        else if (y == 0 || noiseMap[x, y-1] < waterLevel) {
+            return true;
+        }
+        else if (x == 0 || noiseMap[x-1, y] < waterLevel) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
