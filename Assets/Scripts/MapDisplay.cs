84ad18636d1c18ab18e1c7cc1b4ab69d0167e495
Sam Feast <sfeast43@gmail.com>
2024-04-13 16:40:02 +0100

    Beach colouring

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -29,6 +29,7 @@ public class MapDisplay : MonoBehaviour
     }
 
     public void DrawColourMap(float[,] noiseMap, float waterLevel, float beachLevel) {
+
         int width = noiseMap.GetLength(0);
         int height = noiseMap.GetLength(1);
 
@@ -37,6 +38,13 @@ public class MapDisplay : MonoBehaviour
         texture.wrapMode = TextureWrapMode.Clamp;
 
         Color[] colourMap = new Color[width * height];
+
+        Color[] beachColours = new Color[4];
+        beachColours[0] = new Color((float)246 / 255, (float)215 / 255, (float)176 / 255, 1);
+        beachColours[1] = new Color((float)242 / 255, (float)210 / 255, (float)169 / 255, 1);
+        beachColours[2] = new Color((float)236 / 255, (float)204 / 255, (float)162 / 255, 1);
+        beachColours[3] = new Color((float)231 / 255, (float)196 / 255, (float)150 / 255, 1);
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
 
@@ -44,7 +52,8 @@ public class MapDisplay : MonoBehaviour
                     float darkness = Mathf.Max(noiseMap[x, y] / waterLevel + 0.3f, 0.3f);
                     colourMap[y * width + x] = new Color(0f, 0f, darkness, 1);
                 } else if (noiseMap[x, y] < beachLevel) {
-                    colourMap[y * width + x] = Color.yellow;
+                    int rand = Mathf.FloorToInt(Random.value * (3.9999f));
+                    colourMap[y * width + x] = beachColours[rand];
                 } else {
                     colourMap[y * width + x] = Color.green;
                 }
